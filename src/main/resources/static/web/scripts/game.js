339b00a7b479a3266065247aa382ded5a0ece922
teresajuan <teresajuangarcia@gmail.com>
2018-02-01 14:56:14 +0100

    create code to load the correct GamePlayer data

--- src/main/resources/static/web/scripts/game.js
+++ src/main/resources/static/web/scripts/game.js
@@ -1,5 +1,27 @@
 
 
+$.getJSON(relatedUrl("gp"), function(json) {
+    var data = json;
+    console.log(data);
+
+});
+
+/*funciones para devolver el api correspondiente en función del gp seleccionado*/
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function relatedUrl (locationData) {
+
+    return "http://localhost:8080/api/game_view/" + getParameterByName(locationData);
+
+}
+
+/*creacion del grid*/
+
     var columnsTitle = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     var rowsTitle = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     var printGamePlayerGrid1 = document.getElementById('salvoTHead');
